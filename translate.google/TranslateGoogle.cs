88ff428a359020b1c9ea03ce59cc613b332a8c73
marco rafael <marcorafaeldemelo16@gmail.com>
2022-07-04 16:51:49 -0300

    Adicionando validação caso não conseguir ir na internet

--- translate.google/TranslateGoogle.cs
+++ translate.google/TranslateGoogle.cs
@@ -1,19 +1,24 @@
-using Newtonsoft.Json;
-
 namespace Translate.Google
 {
     public class TranslateGoogle : GetTranslateGoogle
     {
 
-        public async Task<string> TranslateTextAsync(string texto, string IdiomaOrigem, string IdiomaDestino)
+        public async Task<string?> TranslateTextAsync(string texto, string IdiomaOrigem, string IdiomaDestino)
         {
-            var tradusir = await GetTranslateAsync(texto, IdiomaOrigem, IdiomaDestino);/*[["texto tradusido"]]*/
-            //remover os primeiros 4 e os últimos 7 caractéres
-            string textFin = tradusir.Substring(2, tradusir.Length - 4);
-            //remover os \\n do texto e adicionar nova linha no lugar
-            textFin = textFin.Replace("\\n", "\n");
+            var tradusir = await GetTranslateAsync(texto, IdiomaOrigem, IdiomaDestino);
+            if (tradusir != null)
+            {
+                //remover os primeiros 4 e os últimos 7 caractéres
+                string textFin = tradusir.Substring(2, tradusir.Length - 4);
+                //remover os \\n do texto e adicionar nova linha no lugar
+                textFin = textFin.Replace("\\n", "\n");
 
-            return textFin;
+                return textFin;
+            }
+            else
+            {
+                return null;
+            }
         }
 
     }
